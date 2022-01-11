30dd3a2b6b572d1901b2917e8fe5126e7c06583f
BlueMarineMoon <minibus2004@naver.com>
2022-01-11 22:01:00 +0900

    보충수업끝

--- src/org/comstudy21/member/model/MemberDAO.java
+++ src/org/comstudy21/member/model/MemberDAO.java
@@ -7,10 +7,18 @@ import java.util.Vector;
 
 public class MemberDAO {
 	private static final Vector<MemberDTO> memList = new Vector<MemberDTO>();
+	private static int sequence = 1;
+	static {
+		memList.add(new MemberDTO(sequence++, "홍길동", "hong@a.com", "010-1111-1111"));
+		memList.add(new MemberDTO(sequence++, "이순신", "lee@a.com", "010-2222-2222"));
+		memList.add(new MemberDTO(sequence++, "일지매", "ilg@a.com", "010-3333-3333"));
+	}
 	
 	public List<MemberDTO> selectAll(){
 		List<MemberDTO> list = new ArrayList<MemberDTO>();
-		Collections.copy(list, memList);
+		for(MemberDTO mem : memList) {
+			list.add((MemberDTO)mem.clone());
+		}
 		return list;
 	}
 	public MemberDTO selectOne(MemberDTO dto) {
@@ -55,4 +63,8 @@ public class MemberDAO {
 		return false;
 	}
 	
+	public void insert(MemberDTO memberDTO) {
+		memberDTO.setIdx(sequence++);
+		memList.add(memberDTO);
+	}
 }
--- src/org/comstudy21/member/resource/R.java
+++ src/org/comstudy21/member/resource/R.java
@@ -1,7 +1,11 @@
 package org.comstudy21.member.resource;
 
+import java.util.Hashtable;
 import java.util.Scanner;
 
+import org.comstudy21.member.controller.Controller;
+import org.comstudy21.member.controller.InputController;
+import org.comstudy21.member.model.MemberDAO;
 import org.comstudy21.member.view.Delete;
 import org.comstudy21.member.view.End;
 import org.comstudy21.member.view.Input;
@@ -20,6 +24,7 @@ public class R {
 	public static final String titleDelete = "::: 삭제 기능 :::";
 	public static final String titleEnd = "::: 프로그램 종료 :::";
 	public static final String titleApp = "::: 회원관리 프로그램 :::";
+	public static final String titleMenu = "::: 메뉴 :::";
 
 	public static final Scanner scan = new Scanner(System.in);
 
@@ -29,6 +34,21 @@ public class R {
 	public static final MemberView modify = new Modify();
 	public static final MemberView delete = new Delete();
 	public static final MemberView end = new End();
+	public static final MemberView menu = new End();
 
 	public static final MainActivity mainActivity = new MainActivity();
+	
+	public static final MemberDAO memDao = new MemberDAO();
+	
+	public static final Hashtable<Integer, Controller> ctrlMapper = new Hashtable<Integer, Controller>();
+	public static final int MENU=0, INPUT=1, LIST=2, SEARCH=3, MODIFY=4, DELETE=5, END=6;
+	static {
+		ctrlMapper.put(MENU, new InputController());
+		ctrlMapper.put(INPUT, new InputController());
+		ctrlMapper.put(LIST, new InputController());
+		ctrlMapper.put(SEARCH, new InputController());
+		ctrlMapper.put(MODIFY, new InputController());
+		ctrlMapper.put(DELETE, new InputController());
+		ctrlMapper.put(END, new InputController());
+	}
 }
